8094efd4196f9993fd4e662ae081d910eb37a280
MyDickTasteLikeLemons <roman.rumba@hotmail.com>
2017-11-10 14:10:01 +0000

    Base commit

---
+++ VideoPlayerScript.js
@@ -0,0 +1,16 @@
+
+class player {
+
+  constructor() {
+	console.log("hi");
+   }
+  load() {
+  }
+
+  
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const p = new player();
+  p.load();
+});
---
+++ script.js
@@ -0,0 +1,16 @@
+
+class MainPage {
+
+  constructor() {
+	console.log("hi");
+   }
+  load() {
+  }
+
+  
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const Main = new MainPage();
+  Main.load();
+});
